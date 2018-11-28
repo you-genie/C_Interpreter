4581777d00a85fbfa6828774a8cbaa68c07edc4a
Yujin Chung <yujinchung863@gmail.com>
2018-11-28 15:02:07 +0000

    [#1] Table Interface Implementation

---
+++ Interpreter/test.py
@@ -0,0 +1,27 @@
+import abc
+
+
+class Table:
+
+    __metaclass__ = abc.ABCMeta
+    
+    @abc.abstactMethod
+    def __str__(self):
+        pass
+
+    @abc.absstractMethod
+    def get_element(self, index):
+        pass
+
+    @abc.abstractMethod
+    def push(self, elem):
+        pass
+    
+    @abc.abstractMethod
+    def pop(self, elem):
+        pass
+    
+    @abc.abstactMethod
+    def print_element(self, index):
+        pass
+    
